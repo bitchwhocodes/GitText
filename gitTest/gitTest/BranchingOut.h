a6b6e9a75ad90871f27487078c16d3005d19810e
Veronique Brossier <veronique@v-ro.com>
2012-07-12 22:08:02 -0400

    branching example

---
+++ gitTest/gitTest/BranchingOut.h
@@ -0,0 +1,13 @@
+//
+//  BranchingOut.h
+//  gitTest
+//
+//  Created by Véronique Brossier on 7/12/12.
+//  Copyright (c) 2012 chez Véronique. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BranchingOut : NSObject
+
+@end
---
+++ gitTest/gitTest/BranchingOut.m
@@ -0,0 +1,13 @@
+//
+//  BranchingOut.m
+//  gitTest
+//
+//  Created by Véronique Brossier on 7/12/12.
+//  Copyright (c) 2012 chez Véronique. All rights reserved.
+//
+
+#import "BranchingOut.h"
+
+@implementation BranchingOut
+
+@end
