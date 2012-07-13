2132f6b5409432362ed89e6ca4389040e23c357f
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2012-07-12 22:52:46 -0400

    made change

--- gitTest/gitTest/ViewController.m
+++ gitTest/gitTest/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"hello Stacey");
+    NSLog(@"hello VRO");
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
